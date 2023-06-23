bfec0c6ea4053d5d1e85f5f2dbc92cdc9d54a3cc
Rajvi Patel <pate4621@sheridancollege.ca>
2023-06-23 13:30:49 -0400

    commit 1 on 1:30

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -20,6 +20,7 @@ public class UserProfile
     private String genre;// the user's preferred genre of movie
     private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
     
+    //commit 1 on 1:30
     /**
      * A constructor that takes in the userID and the favourite genre
      * @param givenID the ID to assign to this user
@@ -30,7 +31,7 @@ public class UserProfile
         userID = givenID;
         genre= givenGenre;
     }
-
+    
     /**
      * A getter for the userID
      * @return the userID
